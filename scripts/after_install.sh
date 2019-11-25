e110874766084b1d50c3c5d27baead72626c4017
Indrayani <maverick@Mavericks-MBP.hsd1.ca.comcast.net>
2019-11-24 20:01:55 -0800

    testing continuous deployment

---
+++ scripts/after_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir /home/ubuntu/Sportify-svc
+cd /home/ubuntu/Sportify-svc
+
+sudo npm install
---
+++ scripts/before_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install node.js
+sudo apt-get update
+sudo apt-get install nodejs -y
+
+# Install forever module 
+sudo npm install forever -g
+
