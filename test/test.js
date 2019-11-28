228ae2d747d62c52e7e5aeb6acb5fa87f008793a
mayurib2 <bhisemayuri@gmail.com>
2019-11-27 19:11:02 -0800

    Update test to get business by business id to use axios

--- test/test.js
+++ test/test.js
@@ -7,13 +7,15 @@ const expect = require('chai').expect;
 require("../index.js");
 
 it('Check get business by business id is working', function(done) {
-  request('http://localhost:3001/businesses/02151fdb-8b50-4068-b8a1-1d24ed425c59', function (error, response, body) {
-    expect(response.statusCode).to.equal(200);
-
-    const bodyJson = JSON.parse(response.body);
-    expect(bodyJson.length).to.be.above(0);
-    done();
-  });
+    axios.get('http://localhost:3001/businesses/02151fdb-8b50-4068-b8a1-1d24ed425c59')
+        .then((res) => {
+            expect(res.status).to.equal(200);
+            done();
+        })
+        .catch((error) => {
+            console.error(error)
+            done();
+        })
 });
 
 it('Check search business by event type is working', function (done) {
