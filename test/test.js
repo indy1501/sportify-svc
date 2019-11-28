2f539980291726bc9aac8c21c8deeaaff638d9d8
mayurib2 <bhisemayuri@gmail.com>
2019-11-27 21:20:17 -0800

    Add test to check update business API

--- test/test.js
+++ test/test.js
@@ -18,7 +18,7 @@ it('Check get business by business id is working', function(done) {
         })
 });
 
-it('Check search business by event type is working', function (done) {
+it('Check search business by event type and city is working', function (done) {
   axios.get('http://localhost:3001/businesses?event_type=restaurant&city=Danville')
       .then((res) => {
         expect(res.status).to.equal(200);
@@ -28,4 +28,16 @@ it('Check search business by event type is working', function (done) {
         console.error(error)
         done();
       })
+});
+
+it('Check update business is working', function (done) {
+    axios.get('localhost:3001/businesses/6284b41e-b61a-4bab-92ad-7cabe7e3201f')
+        .then((res) => {
+            expect(res.status).to.equal(200);
+            done();
+        })
+        .catch((error) => {
+            console.error(error)
+            done();
+        })
 });
